2033234fb9ba9a4f06a9568e0e5f3bf79f386ec9
diishqa <kkover073@gmail.com>
2025-09-15 15:14:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace laba2_HOMEWORK_ZADANIE_10_Kuzmina_ISP_232
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
